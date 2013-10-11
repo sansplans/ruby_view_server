61700ced22ba36738cf45d549fa925e215d477a3
sansplans <imjustsansplans@gmail.com>
2013-10-11 20:33:22 +0100

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,12 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 'hello!'
+template = ERB.new "I want to say: <%=y %>"
+puts template.result(binding)
+
+z = ['dogs', 'cats', 'rabbits']
+
+template = ERB.new "<% z.each do |pet| puts pet end %>"
+puts template.result(binding)
